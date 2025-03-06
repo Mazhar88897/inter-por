e573e04b1f20df79bcdee3f8d6d77415dd77a974
Mazhar Khan <113559751+Mazhar88897@users.noreply.github.com>
2025-03-07 04:35:06 +0500

    hero

---
+++ components/Features/featuresData.tsx
@@ -0,0 +1,49 @@
+import { Feature } from "@/types/feature";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    icon: "/images/icon/icon-01.svg",
+    title: "Market Exposure",
+    description:
+      "ITF Intertex Portugal attracts a wide range of visitors, including retailers, wholesalers, distributors, designers, and industry professionals",
+  },
+  {
+    id: 2,
+    icon: "/images/icon/icon-02.svg",
+    title: "Networking Opportunities",
+    description:
+      "ITF Intertex Portugal serves as a hub for networking and building valuable business connections.",
+  },
+  {
+    id: 3,
+    icon: "/images/icon/icon-03.svg",
+    title: "Market Research and Insights",
+    description:
+      "Exhibiting at ITF Intertex Portugal, you can gather market research and gain insights into the latest industry trends.",
+  },
+  {
+    id: 4,
+    icon: "/images/icon/icon-04.svg",
+    title: "Brand Visibility and Recognition",
+    description:
+      "You can enhance your brand visibility and establish your company's presence in the Portuguese market.",
+  },
+  // {
+  //   id: 5,
+  //   icon: "/images/icon/icon-05.svg",
+  //   title: "Customs and Compliance",
+  //   description:
+  //     "Navigating complex regulations to ensure smooth clearance every time.",
+  // },
+  // {
+  //   id: 6,
+  //   icon: "/images/icon/icon-06.svg",
+  //   title: "Reliable Tracking and Updates",
+  //   description:
+  //     "Stay informed with real-time updates on your shipments.",
+  // },
+];
+
+
+export default featuresData;
