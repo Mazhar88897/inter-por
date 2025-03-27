e97a944d34194eed13cd173f272f20f9fd78579a
Mazhar Khan <113559751+Mazhar88897@users.noreply.github.com>
2025-03-27 06:32:17 +0500

    volume 5

--- components/slider/page.tsx
+++ components/slider/page.tsx
@@ -2,136 +2,41 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 
+const images = [
+    "/images/hero/artistic.png",
+  "/images/hero/mehmood.png",
+  "/images/hero/naveena.png",
+    "/images/hero/sapp.png",
+    "/images/hero/soor.png",
+  
+  
+  "/images/hero/nishat.png",
+  "/images/hero/sadaqat.png",
+ "/images/hero/alkaram.png",
+];
+
 const EventList = () => {
   return (
     <>
-      <div className="relative my-5 lg:my-14 overflow-hidden border-b border-t text-black dark:text-white py-3">
-        <motion.div
-          className="flex space-x-10 whitespace-nowrap"
-          animate={{ x: ["100%", "-100%"] }}
-          transition={{ repeat: Infinity, duration: 30, ease: "easeInOut" }} // Slower & smoother
-        >
-          <div className="flex gap-1 justify-center items-center">
-            <p className="text-lg font-bold">
-            Sapphire Textile Mills 
-            </p>
-            <p className="pl-2 text-lg text-yellow-600 font-semibold">
-            ★★★★★ 
-            </p>
-            <ImagePak />
-            
-            
-          </div>
-          <div className="flex gap-1 justify-center items-center">
-            <p className="text-lg font-bold">
-            Mehmood Group of Industries
-            </p>
-            <p className=" pl-2 text-lg text-yellow-600 font-semibold">
-            ★★★★★ 
-            </p>
-            <ImagePak />
-            
-            
-          </div>
-
-          <div className="flex gap-1 justify-center items-center">
-            <p className="text-lg font-semibold">
-            Nishat Chunian Ltd.
-            </p>
-            <p className="text-lg text-yellow-600 font-semibold pl-2">
-            ★★★★★ 
-            </p>
-            <ImagePak />
-            
-            
-          </div>
-
-          <div className="flex gap-1 justify-center items-center">
-            <p className="text-lg font-semibold">
-            Artistic Milliners
-            </p>
-            <p className="text-lg text-yellow-600 font-semibold pl-2">
-            ★★★★★ 
-            </p>
-            <ImagePak />
-            
-            
-          </div>
-
-          <div className="flex gap-1 justify-center items-center">
-            <p className="text-lg font-semibold">
-            Sapphire Textile Mills 
-            </p>
-            <p className="text-lg text-yellow-600 font-semibold pl-2">
-            ★★★★★ 
-            </p>
-            <ImagePak />
-            
-            
-          </div>
-
-          <div className="flex gap-1 justify-center items-center ">
-            <p className="text-lg font-semibold">
-            Soorty
-            </p>
-            <p className="text-lg text-yellow-600 font-semibold pl-2">
-            ★★★★★ 
-            </p>
-            <ImagePak />
-            
-            
-          </div>
-          <div className="flex gap-1 justify-center items-center">
-            <p className="text-lg font-semibold">
-         Naveena
-            </p>
-            <p className="text-lg text-yellow-600 font-semibold pl-2">
-            ★★★★★ 
-            </p>
-            <ImagePak />
-            
-          </div>
-          <div className="flex gap-1 justify-center items-center">
-            <p className="text-lg font-semibold">
-            Sadaqat Group
-            </p>
-            <p className="text-lg text-yellow-600 font-semibold pl-2">
-            ★★★★★ 
-            </p>
-           
-            <ImagePak />
-            
-          </div>
-          <div className="flex gap-1 justify-center items-center">
-            <p className="text-lg font-semibold">
-            Al Karam Towel
-            </p>
-            <p className="text-lg text-yellow-600 font-semibold pl-2">
-            ★★★★★ 
-            </p>
-           <ImagePak />
-            
-          </div>
- 
-
-          
-        </motion.div>
-      </div>
+    <div className="relative my-5 lg:my-14 overflow-hidden border-b border-t text-black dark:text-white py-3">
+      <motion.div
+        className="flex space-x-10 whitespace-nowrap"
+        animate={{ x: ["100%", "-100%"] }}
+        transition={{ repeat: Infinity, duration: 30, ease: "easeInOut" }} // Slower & smoother
+      >
+        {images.map((src, index) => (
+          <div key={index} className="w-40 h-40 lg:w-60 lg:h-60 relative flex-shrink-0">
+           <Image 
+              src={src} 
+              alt={`Event ${index + 1}`} 
+              fill 
+              className="object-cover" 
+            />    </div>
+        ))}
+      </motion.div>
+    </div>
     </>
   );
 };
 
 export default EventList;
-
-
-import React from 'react'
-
-const ImagePak = () => {
-  return (
-    <div> <img
-    src="https://upload.wikimedia.org/wikipedia/commons/3/32/Flag_of_Pakistan.svg"
-    alt="USA Flag"
-    width="20"
-  /></div>
-  )
-}
